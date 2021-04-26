2c6813ac71c9186bcde3820a7244491da844d59d
Joel Truher <joel@truher.org>
2021-04-26 08:22:48 -0700

    add table of cumulative data

--- static/timeseries.js
+++ static/timeseries.js
@@ -11,7 +11,9 @@ const container = document.querySelector('#timeseries');
     const chart = fc.chartCartesian(d3.scaleTime(), d3.scaleLinear())
       .xDomain(fc.extentTime().pad([0.1, 0.1]).accessors([d=>new Date(d[0]/1e6)])(data))
       .yDomain(fc.extentLinear().include([0]).pad([0.0, 0.1]).accessors([d=>Number(d[2])])(data))
-      .chartLabel('foo')
+      .chartLabel('Volume in microliters by minute')
+      .xLabel('Time (minute buckets)')
+      .yLabel('Volume (microliters)')
       .yOrient('left')
       .svgPlotArea(line);
 
