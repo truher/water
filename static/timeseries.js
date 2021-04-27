ab92df00c23e825e9cf8004a1a79395daf151637
Joel Truher <joel@truher.org>
2021-04-27 16:38:11 -0700

    cleaner charts

--- static/timeseries.js
+++ static/timeseries.js
@@ -4,10 +4,10 @@ d3.json('/data_by_sec').then(function(data) {
     .mainValue(d=>Number(d[2]));
 
   const chart = fc.chartCartesian(d3.scaleTime(), d3.scaleLinear())
-    .xDomain(fc.extentTime().pad([0.1, 0.1]).accessors([d=>new Date(d[0]/1e6)])(data))
-    .yDomain(fc.extentLinear().include([0]).pad([0.0, 0.1]).accessors([d=>Number(d[2])])(data))
+    .xDomain(fc.extentTime().pad([0.025, 0.025]).accessors([d=>new Date(d[0]/1e6)])(data))
+    .yDomain(fc.extentLinear().include([0]).pad([0.0, 0.025]).accessors([d=>Number(d[2])])(data))
     .chartLabel('Volume in microliters by second')
-    .xLabel('Time (minute buckets)')
+    .xLabel('Time (one second buckets)')
     .yLabel('Volume (microliters)')
     .yOrient('left')
     .svgPlotArea(line);
@@ -33,10 +33,10 @@ d3.json('/data_by_min').then(function(data) {
     .mainValue(d=>Number(d[2]));
 
   const chart = fc.chartCartesian(d3.scaleTime(), d3.scaleLinear())
-    .xDomain(fc.extentTime().pad([0.1, 0.1]).accessors([d=>new Date(d[0]/1e6)])(data))
-    .yDomain(fc.extentLinear().include([0]).pad([0.0, 0.1]).accessors([d=>Number(d[2])])(data))
+    .xDomain(fc.extentTime().pad([0.025, 0.025]).accessors([d=>new Date(d[0]/1e6)])(data))
+    .yDomain(fc.extentLinear().include([0]).pad([0.0, 0.025]).accessors([d=>Number(d[2])])(data))
     .chartLabel('Volume in microliters by minute')
-    .xLabel('Time (minute buckets)')
+    .xLabel('Time (one minute buckets)')
     .yLabel('Volume (microliters)')
     .yOrient('left')
     .svgPlotArea(line);
