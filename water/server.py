0812025149132c6e47500e9b66a27d6c5fa860de
Joel Truher <joel@truher.org>
2021-04-30 17:52:51 -0700

    turn down logging

--- water/server.py
+++ water/server.py
@@ -12,7 +12,7 @@ from writer import DataWriter
 import spi
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%S',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 app = Flask(__name__)
 
