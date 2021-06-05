41c74c495907efd33c7bfd945c6b1801cf0d1dd5
Joel Truher <joel@truher.org>
2021-06-05 13:10:13 -0700

    realistic data, relu conv => works pretty well

--- analysis/disag_toy.py
+++ analysis/disag_toy.py
@@ -46,15 +46,55 @@ def make_random_data(classes, events, samples):
     df['mains'] = np.maximum(df['mains'], 0)  # negative noise is nonphysical, don't try to predict it
     return df, loads
 
-kernel_size = 5
-batch_size = 11
-classes = 8
-events = 20
-samples = 2000
+def make_realistic_data():
+    samples = 86400
+
+    df = pd.DataFrame()
+    df['sprinkler'] = np.zeros(samples)
+    df['front'] = np.zeros(samples)
+    df['shower'] = np.zeros(samples)
+    df['drip'] = np.zeros(samples)
+    df['toilet'] = np.zeros(samples)
+    df['faucet'] = np.zeros(samples)
+
+    # this is modeled after jun 2
+    df['shower'][27420:28140] = 1    #  7:37-7:49
+    df['shower'][33900:34320] = 1    #  9:25-9:32
+    df['toilet'][33720:33755] = 1    #  9:22 35s
+    df['toilet'][33780:33815] = 1    #  9:23 35s
+    df['toilet'][33840:33875] = 1    #  9:24 35s
+    df['front'][36180:37260] = 1     # 10:03-10:21
+    df['drip'][39600:43200] = 1      # 11:00-12:00
+    df['toilet'][58140:58175] = 1    # 16:09 35s
+    df['toilet'][58200:58235] = 1    # 16:10 35s
+    df['toilet'][80520:80555] = 1    # 22:22 35s
+    df['faucet'][80580:80585] = 1    # 22:23 5s
+    df['faucet'][80760:80775] = 1    # 22:26 15s
+    df['toilet'][81300:81335] = 1    # 22:35 35s
+    df['faucet'][81420:81435] = 1    # 22:37 15s
+    df['sprinkler'][81900:84600] = 1 # 22:30-23:30
+
+    loads = [7.1, 4.4, 2.75, 0.9, 3.05, 1.0]
+    df['mains'] = df.dot(loads)
+    df['mains'] = df['mains'] + np.random.normal(0, 0.05, samples)
+    df['mains'] = np.maximum(df['mains'], 0)  # negative noise is nonphysical, don't try to predict it
+    return df, loads
+
+kernel_size = 5 # please be odd
+#batch_size = 111
+batch_size = 111
+#classes = 8
+#events = 20
+#samples = 2000
+#df, loads = make_random_data(classes, events, samples)
+#df = make_data()
+
+df, loads = make_realistic_data()
+classes = len(df.columns) - 1
+samples = len(df.index)
+
 reshape_samples = (batch_size * kernel_size) * (samples // (batch_size * kernel_size))
 print(reshape_samples)
-df,loads = make_random_data(classes, events, samples)
-#df = make_data()
 
 print("df")
 print(df[0:10])
@@ -78,14 +118,23 @@ y = df.iloc[:,:-1].values
 #print(y[0:10])
 
 # batches of 11 timesteps
-i = tf.keras.Input(shape=(11,1),
+# make this MUCH wider, try 111
+i = tf.keras.Input(shape=(batch_size,1),
     name="input_layer")
 
 # number of filters, 17, is arbitrary, just different from all the other numbers
 # kernel is window width
 # 7 windows of 5 fit in a batch of 11
-conv = tf.keras.layers.Conv1D(filters=17, kernel_size=5, activation='linear',
+conv = tf.keras.layers.Conv1D(filters=17, kernel_size=kernel_size, activation='relu',
     name="conv_layer")
+#pool = tf.keras.layers.MaxPool1D(pool_size=13,
+    #name="pool_layer")
+conv_2 = tf.keras.layers.Conv1D(filters=19, kernel_size=5, activation='relu',
+    name="conv_layer_2")
+#pool_2 = tf.keras.layers.MaxPool1D(pool_size=13,
+    #name="pool_layer_2")
+conv_3 = tf.keras.layers.Conv1D(filters=23, kernel_size=5, activation='relu',
+    name="conv_layer_3")
 
 c = tf.keras.layers.Dense(units=classes*10, activation='relu',
     name="middle_layer")
@@ -106,7 +155,8 @@ mo = tf.keras.layers.Dense(units=1,
     bias_constraint=tf.keras.constraints.MinMaxNorm(min_value=0.0, max_value=0.0, rate=1.0),
     name="mains_output")
 
-conv_branch = conv(i)
+#conv_branch = conv_3(pool_2(conv_2(pool(conv(i)))))
+conv_branch = conv_3(conv_2(conv(i)))
 internal_branch = c(conv_branch)
 category_branch = o(internal_branch)
 mains_branch = mo(category_branch)
@@ -127,7 +177,7 @@ print("train classifier ...")
 
 # batches of 11
 #xtrain = x[0:891].reshape(-1,11,1)
-xtrain = x[0:reshape_samples].reshape(-1,11,1)
+xtrain = x[0:reshape_samples].reshape(-1,batch_size,1)
 print("xtrain")
 print(xtrain.shape)
 #print(xtrain)
@@ -135,10 +185,11 @@ print(xtrain.shape)
 # each span of 5 predicts the features at the center sample
 #yctrain = y[0:891].reshape(-1,11,2)[:,2:-2]
 #yctrain = y[0:891].reshape(-1,11,classes)[:,2:-2]
-yctrain = y[0:reshape_samples].reshape(-1,11,classes)[:,2:-2]
+conv_layers = 3
+yctrain = y[0:reshape_samples].reshape(-1,batch_size,classes)[:, conv_layers * (kernel_size//2):-(conv_layers * (kernel_size//2))]
 shaped_training = yctrain.reshape(-1,classes).astype(int)
 #ymtrain = x[0:891].reshape(-1,11,1)[:,2:-2]
-ymtrain = x[0:reshape_samples].reshape(-1,11,1)[:,2:-2]
+ymtrain = x[0:reshape_samples].reshape(-1,batch_size,1)[:, conv_layers * (kernel_size//2):-(conv_layers * (kernel_size//2))]
 print("yctrain")
 print(yctrain.shape)
 #print(yctrain)
@@ -150,7 +201,7 @@ print(ymtrain.shape)
 print(m.summary())
 tb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard_log/classifier", histogram_freq=1)
 # note x in both the "in" and "out" positions here
-m.fit(xtrain, [yctrain, ymtrain], batch_size=11, epochs=5000, verbose=0, callbacks=[tb])
+m.fit(xtrain, [yctrain, ymtrain], batch_size=batch_size, epochs=1000, verbose=0, callbacks=[tb])
 
 
 #y1 = m.predict(xtrain)
@@ -163,6 +214,8 @@ print("train mains output ...")
 
 i.trainable = False
 conv.trainable = False
+conv_2.trainable = False
+conv_3.trainable = False
 c.trainable = False
 o.trainable = False
 loss_weights = { "category_output": 0.0, "mains_output": 1.0 }
@@ -170,7 +223,7 @@ m.compile(loss=losses, loss_weights=loss_weights, optimizer='adam')
 
 print(m.summary()) # should show only 3 trainable params, but since bias is constrained it's actually only two
 tb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard_log/mains", histogram_freq=1)
-m.fit(xtrain, [yctrain, ymtrain], batch_size=11, epochs=2000, verbose=0, callbacks=[tb])
+m.fit(xtrain, [yctrain, ymtrain], batch_size=batch_size, epochs=400, verbose=0, callbacks=[tb])
 
 print("done training!")
 
@@ -227,8 +280,23 @@ print(sklearn.metrics.mean_squared_error(ymtrain.reshape(-1), m1.reshape(-1)))
 print("confusion matrices")
 print(sklearn.metrics.multilabel_confusion_matrix(shaped_training, shaped_c1.astype(int)))
 
-plt.plot(ymtrain.reshape(-1))
-plt.plot(m1.reshape(-1))
-plt.plot(shaped_c1)
-plt.title("mains prediction")
+tdf = pd.DataFrame(shaped_training, columns=df.columns[:-1])
+c1df = pd.DataFrame(shaped_c1, columns=df.columns[:-1])
+
+c1_main = c1df.dot(loads)
+
+fix,axs = plt.subplots(classes+1, sharex=True)
+
+#plt.plot(ymtrain.reshape(-1), label='mains observed')
+#plt.plot(m1.reshape(-1), label='mains predicted')
+axs[0].plot(ymtrain.reshape(-1), label='mains observed')
+axs[0].plot(c1_main, label='mains predicted using known loads')
+axs[0].plot(m1.reshape(-1), label='mains predicted using predicted loads')
+axs[0].legend(loc='upper left')
+#plt.plot(shaped_c1)
+for i,col in enumerate(c1df):
+    axs[i+1].plot(tdf[col], label=col+' observed')
+    axs[i+1].plot(c1df[col], label=col+' predicted')
+    axs[i+1].legend(loc='upper left')
+#plt.legend()
 plt.show()
